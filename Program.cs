63436dd538d7a6983c6440a3bf44c8d8578e6c17
Egor <EgorrKosolapoff@ya.ru>
2023-07-22 19:06:46 +0700

    third task

--- Program.cs
+++ Program.cs
@@ -49,3 +49,37 @@ int[] array = new int[n];
 InputArray(array);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 ReleaseArray(array); */
+
+
+/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76 */
+
+
+
+/* void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 100);
+}
+void ReleaseArray(int[] array)
+{
+    int n_max = 0;
+    int n_min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > n_max)
+            n_max = array[i];
+        if (array[i] < n_min)
+            n_min = array[i];
+
+    }
+    Console.WriteLine($"[{string.Join(", ", array)}] => {n_max} - {n_min} = {n_max - n_min}");
+}
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+ReleaseArray(array); */
+
