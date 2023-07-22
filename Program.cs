5da40fd6c9029938ffd8dd33ed2dbfc7924ea8db
Egor <EgorrKosolapoff@ya.ru>
2023-07-22 18:47:30 +0700

    second task

--- Program.cs
+++ Program.cs
@@ -22,3 +22,30 @@ InputArray(array);
 Console.WriteLine($"[{string.Join(", ", array)}]");
 ReleaseArray(array);
  */
+
+
+
+/*  Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+/* void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11);
+}
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        count = count + array[i];
+    Console.WriteLine(count);
+}
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array); */
